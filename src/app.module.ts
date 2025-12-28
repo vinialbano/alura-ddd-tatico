5e315d0b610d567ca8e1bfa4702009bde5f3e502
Vinicius Albano <vinialbano@gmail.com>
2025-12-28 12:05:29 -0300

    refactor: remove unused AppController and AppService from the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CartModule } from './infrastructure/modules/cart.module';
 
 @Module({
   imports: [CartModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
