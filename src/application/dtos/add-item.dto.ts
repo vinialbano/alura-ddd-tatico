99a0d44edb54eb1274261e1cb2e755a35229a79a
Vinicius Albano <vinialbano@gmail.com>
2025-12-27 21:57:00 -0300

    feat(US1): create DTOs for cart creation and adding items

---
+++ src/application/dtos/add-item.dto.ts
@@ -0,0 +1,9 @@
+/**
+ * AddItemDto
+ *
+ * Input DTO for adding an item to a shopping cart
+ */
+export class AddItemDto {
+  productId: string;
+  quantity: number;
+}
---
+++ src/application/dtos/create-cart.dto.ts
@@ -0,0 +1,8 @@
+/**
+ * CreateCartDto
+ *
+ * Input DTO for creating a new shopping cart
+ */
+export class CreateCartDto {
+  customerId: string;
+}
