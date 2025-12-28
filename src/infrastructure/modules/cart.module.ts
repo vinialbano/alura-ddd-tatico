fc15551f7ad6ebe210dc5c75897ce46a71980041
Vinicius Albano <vinialbano@gmail.com>
2025-12-27 22:18:08 -0300

    feat(US1): wire CartModule dependencies

---
+++ src/infrastructure/modules/cart.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { CartController } from '../controllers/cart.controller';
+import { CartService } from '../../application/services/cart.service';
+import { InMemoryShoppingCartRepository } from '../repositories/in-memory-shopping-cart.repository';
+
+/**
+ * CartModule
+ *
+ * NestJS module that wires together the cart feature
+ * Controller → Service → Repository
+ */
+@Module({
+  controllers: [CartController],
+  providers: [
+    CartService,
+    {
+      provide: 'ShoppingCartRepository',
+      useClass: InMemoryShoppingCartRepository,
+    },
+    {
+      provide: CartService,
+      useFactory: (repository: InMemoryShoppingCartRepository) => {
+        return new CartService(repository);
+      },
+      inject: ['ShoppingCartRepository'],
+    },
+  ],
+  exports: [CartService],
+})
+export class CartModule {}
