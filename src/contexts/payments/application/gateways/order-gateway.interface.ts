6157fadb7f3b21569c755d94373dc6f39678ef27
Vinicius Albano <vinialbano@gmail.com>
2026-01-08 11:17:45 -0300

    feat(payments): add OrderGateway interface

---
+++ src/contexts/payments/application/gateways/order-gateway.interface.ts
@@ -0,0 +1,9 @@
+export interface IOrderGateway {
+  /**
+   * Mark an order as paid
+   * Called by Payments BC after successful payment processing
+   */
+  markOrderAsPaid(orderId: string, paymentId: string): Promise<void>;
+}
+
+export const ORDER_GATEWAY = Symbol('ORDER_GATEWAY');
